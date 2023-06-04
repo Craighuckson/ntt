e54284f1c0a510ba0b4e6c16f4fd286d113a44cd
Craig Huckson <craig.huckson@gmail.com>
2023-06-04 14:36:16 -0400

    Completed project 4

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,53 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+// Q: How to implement multiplication only using addition?
+// A: Use the fact that multiplication is repeated addition.
+
+// Q: How to implement repeated addition?
+// A: Use the fact that repeated addition is repeated incrementation.
+
+// Q: How to implement repeated incrementation?
+// A: Use the fact that incrementation is adding 1.
+
+// pseudocode:
+
+// R2 = 0
+// while (R1 > 0) {
+//   R2 = R2 + R0
+//   R1 = R1 - 1
+// }
+
+// assembly:
+
+@R2
+M=0
+
+(LOOP)
+@R1
+D=M
+
+@END
+D;JEQ
+//Q: what does the line above do?
+//A: if R1 == 0, jump to END
+
+
+@R0
+D=M
+
+@R2
+M=M+D
+
+@R1
+M=M-1
+
+@LOOP
+0;JMP
+
+
+(END)
+@END
+0;JMP
+
